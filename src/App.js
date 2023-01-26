9180da27cf1b9f6ec786dd0711c38577661d3e58
mert-ozturk <mertozturk.seo.yonetim@gmail.com>
2023-01-26 13:14:16 +0300

    test2

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [name,setName] = useState("Mehmet");
+  const [age,setAge] = useState(29);
+  const [friends,setFriends] = useState(["Ayşe","Fatma"]);
   return (
-    <div>
-      
+    <div className='App'>
+      <h1> {name}</h1>
+      <h2>{age}</h2>
+      <button onClick={()=> setName("Hariye")}>Change name</button>
+      <button onClick={()=> setAge(25)}>Change age</button>
+      <hr />
+      <br></br>
+    <h2>Friends</h2>
+    {friends.map((friend,index)=>(
+      <div key={index}>{friend}</div>
+    ))}
+    <button onClick={()=> setFriends([...friends,"Hayriye"])}>add new friend</button>
+    
     </div>
   )
 }
